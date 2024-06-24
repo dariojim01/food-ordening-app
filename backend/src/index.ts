382a1079a1ea32e79f6322fc518db8d807907978
Dario <dariojim01@gmail.com>
2024-06-23 22:58:29 -0500

    back: MyUser route and controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import MyUserRoute from "./routes/MyUserRoute";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,9 +15,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get("/test", (req, res) =>{
-    res.send("Hello world");
-})
+app.use("/api/my/user/", MyUserRoute);
 
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`);
